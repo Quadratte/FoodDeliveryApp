93a87cf8d008a85cad1d98af065c70e076ef2fdb
Sergei Philippov <quadratte@gmail.com>
2026-05-05 00:09:57 +0300

    Added basic coordinator pattern structure

---
+++ FoodDeliveryApp/NavigationLayer/Coordinators/Coordinator.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+enum CoordinatorType {
+    case app
+    case onboarding
+    case home
+    case order
+    case list
+    case profile
+}
+
+protocol CoordinatorProtocol: AnyObject {
+    var childCoordinators: [CoordinatorProtocol] { get set }
+    var type: CoordinatorType { get }
+}
--- FoodDeliveryApp/Resources/AppColors.swift
+++ FoodDeliveryApp/Resources/AppColors.swift
@@ -3,4 +3,5 @@ import UIKit
 
 enum AppColors {
     static let accentOrange = UIColor().hex(0xD35400)
+    static let gray = UIColor().hex(0xECF001)
 }
